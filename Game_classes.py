8f98d284514175738e0b07c4322de84a319f70e9
Cactiw <knyazev1514@gmail.com>
2018-10-18 14:04:58 +0300

    Start of development

--- Game_classes.py
+++ Game_classes.py
@@ -1,1 +1,23 @@
-class User()
+class User:
+    id = 0
+    username = ""
+
+    def __init(self, id, username):
+        self.id = id
+        self.username = username
+
+
+
+class Equipment:
+
+
+    def __init__(self, id, name,  place, endurance, power, armor, intelligence, accuracy, agility):
+        self.id = id
+        self.name = name
+        self.place = place
+        self.endurance = endurance
+        self.power = power
+        self.armor = armor
+        self.intelligence = intelligence
+        self.accuracy = accuracy
+        self.agility = agility
