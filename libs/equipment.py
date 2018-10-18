636a7a96c43e01fc0ce438e63f3a4cf450b29d0b
Werozel <khaibulaev.mn@gmail.com>
2018-10-18 21:38:24 +0300

    Разделил по файлам разные классы

---
+++ libs/equipment.py
@@ -0,0 +1,13 @@
+class Equipment:
+
+    def __init__(self, id, name,  place, endurance, power, armor, intelligence, accuracy, agility):
+        self.id = id
+        self.name = name
+        self.place = place
+        self.endurance = endurance
+        self.power = power
+        self.armor = armor
+        self.intelligence = intelligence
+        self.accuracy = accuracy
+        self.agility = agility
+
