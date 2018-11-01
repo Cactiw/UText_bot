1f6bb96522b58ea953c46d5b085384d12076b96c
Werozel <36276118+Werozel@users.noreply.github.com>
2018-11-01 11:05:00 +0300

    Добавил класс столицу, замок гильдий, локации

---
+++ libs/locations/capital.py
@@ -0,0 +1,6 @@
+class Capital:
+	
+	def __init__(self, id):
+		self.id = id
+		self.buildings = {"castle" : 1, "barracs" : 1, "tavern" : 1}
+    
---
+++ libs/locations/guild_castle.py
@@ -0,0 +1,5 @@
+class Guild_Castle:
+	
+	def __init__(self, id):
+		self.id = id
+		self.buildings = {"forge" : 1, "alchemy_station": 1, "enchantment_station" : 1}
