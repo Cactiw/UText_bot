166351ca0724026b22f40369b53fc60201b88c47
Werozel <khaibulaev.mn@gmail.com>
2018-11-05 12:32:15 +0300

    Исправил

--- bin/service_commands.py
+++ bin/service_commands.py
@@ -15,7 +15,7 @@ def sql(bot, update, user_data):
         return
     conn.commit()
     row = cursor.fetchone()
-    response = "Что-то пошло не так в базе данных"
+    response = "Результат запроса: "
     while row:
         for i in range(0, len(row)):
             response += str(row[i]) + " "
