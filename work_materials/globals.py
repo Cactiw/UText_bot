f07dc57de0df0bbe192de92c70bf8af419372040
Werozel <36276118+Werozel@users.noreply.github.com>
2018-11-08 09:10:38 +0300

    Вынес players и players_need_update в globals

--- work_materials/globals.py
+++ work_materials/globals.py
@@ -6,6 +6,9 @@ updater = Updater(token='757939309:AAE3QMqbT8oeyZ44es-l6eSzxpy1toCf_Bk')
 
 dispatcher = updater.dispatcher
 
+players = {}
+players_need_update = Queue()
+
 #Подключаем базу данных, выставляем кодировку
 print("Enter password for database:")
 passwd = 'fiP3Gahz'
