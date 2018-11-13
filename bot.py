2e9c0524dff5409d5e524351ab374a0a6f43e835
Cactiw <knyazev1514@gmail.com>
2018-11-13 18:44:02 +0300

    Добавил функцию удаления самого себя отовсюду из бота по команде /delete_self

--- bot.py
+++ bot.py
@@ -200,6 +200,7 @@ dispatcher.add_handler(MessageHandler(Filters.text and filter_nickname_select, n
 #Команды для админов
 dispatcher.add_handler(CommandHandler("setstatus", set_status, pass_user_data=True, filters = filter_is_admin, pass_args=True))
 dispatcher.add_handler(CommandHandler("sql", sql, pass_user_data=True, filters = filter_is_admin))
+dispatcher.add_handler(CommandHandler("delete_self", delete_self, pass_user_data=True, filters = filter_is_admin))
 dispatcher.add_handler(CommandHandler("showdata", show_data, pass_user_data=True, filters=filter_is_admin))
 
 #Фильтр для вывода инфаормации об игроке
