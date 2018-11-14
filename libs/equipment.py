0314385ab35f00665a764c01ba6cfe84630ac793
Cactiw <Knyazev1514@gmail.com>
2018-11-14 14:58:38 +0300

    Написал equip и unequip

--- libs/equipment.py
+++ libs/equipment.py
@@ -4,11 +4,7 @@ class Equipment(Item):
 
     def __init__(self, type, id, name,  place, endurance, power, armor, intelligence, accuracy, agility):
         super(Equipment, self).__init__(type, id)
+
         self.name = name
         self.place = place
-        self.endurance = endurance
-        self.power = power
-        self.armor = armor
-        self.intelligence = intelligence
-        self.accuracy = accuracy
-        self.agility = agility
+        self.stats = {'endurance': 0, 'power': 0, 'armor': 0, 'agility': 0, 'mana_points': 0}
