eafa367f6e1b6d4575956ffb887f0cfda4c99a55
Cactiw <knyazev1514@gmail.com>
2018-12-19 14:26:34 +0300

    Правки в новом боте

--- libs/bot_async_messaging.py
+++ libs/bot_async_messaging.py
@@ -1,7 +1,14 @@
 from telegram import Bot
+from telegram.utils.request import Request
 
 
 class AsyncBot(Bot):
 
-    def __init__(self, token):
-        super(AsyncBot, self).__init__(token=token)
+    def __init__(self, token, workers = 4, request_kwargs = None):
+        if request_kwargs is None:
+            request_kwargs = {}
+        con_pool_size = workers + 4
+        if 'con_pool_size' not in request_kwargs:
+            request_kwargs['con_pool_size'] = con_pool_size
+        self._request = Request(**request_kwargs)
+        super(AsyncBot, self).__init__(token=token, request=self._request)
