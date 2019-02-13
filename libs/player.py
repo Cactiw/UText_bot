a8bd2b397d1814685ee709d5d83e35afe7c771f4
Cactiw <knyazev1514@gmail.com>
2019-02-13 15:40:47 +0300

    Еще принты

--- libs/player.py
+++ libs/player.py
@@ -64,6 +64,8 @@ class Player:
 
     def update_skills(self):
         class_skills = skills.get(self.game_class)
+        print('class_skills in update_skills -', class_skills)
+        print('list -', list(class_skills.values()))
         for i in list(class_skills.values()):
             if i.name == 'Атака' or i.name == 'Пропуск хода':
                 continue
