9ada5a46c59b83c5d5777b6f3f7a3a96d890564c
Cactiw <knyazev1514@gmail.com>
2019-02-16 16:45:55 +0300

    Починил сраную табуляцию с компов в машзале

--- bin/interprocess_monitor.py
+++ bin/interprocess_monitor.py
@@ -1,4 +1,4 @@
-from work_materials.globals import pending_battles, dispatcher
+from work_materials.globals import pending_battles, dispatcher, players_need_update
 from bin.player_service import get_player, update_status
 from libs.interprocess_dictionaty import InterprocessDictionary, interprocess_queue
 from bin.show_general_buttons import show_general_buttons
@@ -51,47 +51,47 @@ def interprocess_monitor():
             if "stunned" in list_user_data:
                 user_data.pop('stunned')
         elif data.type == "change_player_state":
-			player = get_player(data.data[0])
-			list_keys = list(data.data[1:])
-			for key in list_keys:
-				change_value = data.data.get(key)
-				if key == "exp":
-					player.exp += change_value
-					player.lvl_check()
-				elif key == "dead":
-					player.dead = change_value
-				elif key == "resourses":
-					for new_key in change_value:
-						delta = change_value.get(new_key)
-						old_value = player.resourses.get(new_key)
-						if old_value is None:
-							continue
-						old_value += delta
-						player.resouses.update({new_key : old_value})
-				elif key == "eq_backpack":
-					for new_key in change_value:
-						delta = change_value.get(new_key)
-						old_value = player.eq_backpack.get(new_key)
-						if old_value is None:
-							continue
-						old_value += delta
-						player.resouses.update({new_key : old_value})
-				elif key == "al_backpack":
-					for new_key in change_value:
-						delta = change_value.get(new_key)
-						old_value = player.al_backpack.get(new_key)
-						if old_value is None:
-							continue
-						old_value += delta
-						player.resouses.update({new_key : old_value})	
-				elif key == "res_backpack":
-					for new_key in change_value:
-						delta = change_value.get(new_key)
-						old_value = player.res_backpack.get(new_key)
-						if old_value is None:
-							continue
-						old_value += delta
-						player.resouses.update({new_key : old_value})
-				players_need_update.put(player)
+            player = get_player(data.data[0])
+            list_keys = list(data.data[1:])
+            for key in list_keys:
+                change_value = data.data.get(key)
+                if key == "exp":
+                    player.exp += change_value
+                    player.lvl_check()
+                elif key == "dead":
+                    player.dead = change_value
+                elif key == "resourses":
+                    for new_key in change_value:
+                        delta = change_value.get(new_key)
+                        old_value = player.resourses.get(new_key)
+                        if old_value is None:
+                            continue
+                        old_value += delta
+                        player.resouses.update({new_key : old_value})
+                elif key == "eq_backpack":
+                    for new_key in change_value:
+                        delta = change_value.get(new_key)
+                        old_value = player.eq_backpack.get(new_key)
+                        if old_value is None:
+                            continue
+                        old_value += delta
+                        player.resouses.update({new_key : old_value})
+                elif key == "al_backpack":
+                    for new_key in change_value:
+                        delta = change_value.get(new_key)
+                        old_value = player.al_backpack.get(new_key)
+                        if old_value is None:
+                            continue
+                        old_value += delta
+                        player.resouses.update({new_key : old_value})    
+                elif key == "res_backpack":
+                    for new_key in change_value:
+                        delta = change_value.get(new_key)
+                        old_value = player.res_backpack.get(new_key)
+                        if old_value is None:
+                            continue
+                        old_value += delta
+                        player.resouses.update({new_key : old_value})
+                players_need_update.put(player)
         data = interprocess_queue.get()
     return 0
